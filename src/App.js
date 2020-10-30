b84b1fefc3d44583b1e77baf3dc215fa88121864
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-30 14:18:43 +0100

    Updated App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router} from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import Navbar from './components/navbar';
+import EditObjectName from './components/edit-objectname';
+import ImagesList from './components/images-list';
+import AnnotationsList from './components/annotations-list';
+import DisplayAnnotationsBox from './components/display-annotationsbox';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <div className="container-fluid">
+
+          <div className="row">
+            
+            <div className="col-sm-3 col-md-2 sidebar" id="right_bar">
+              <ul className="nav nav-sidebar"> 
+                <EditObjectName />               
+              </ul>
+              <ul className="nav nav-sidebar">
+                <ImagesList />
+              </ul>
+            </div>
+
+            <DisplayAnnotationsBox />
+
+            <div className="col-md-3" id="right_bar" >
+              <AnnotationsList />
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </Router>
   );
 }
 
