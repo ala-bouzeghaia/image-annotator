ff2d72bc793e074d26aa90256b000a7f1f2c3497
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-30 14:19:26 +0100

    Added component

---
+++ src/components/annotations-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AnnotationsList = () => {
+    return(
+        <div id="annotated_box"> 
+            <h5>Annotations: </h5>
+        </div>
+    );
+};
+
+export default AnnotationsList;
