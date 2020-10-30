63de0b33d1be24827ef52ef6500acdc5aa99f836
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-30 14:19:43 +0100

    Added component

---
+++ src/components/images-list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ImagesList = () => {
+    return(
+        <div id="next_images">
+            <h5>Images: </h5>
+        </div>
+    );
+};
+
+export default ImagesList;
