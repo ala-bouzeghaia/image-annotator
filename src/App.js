bcd6715dcd0dd378da819befb162488acce52177
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-30 17:59:37 +0100

    Made page responsive

--- src/App.js
+++ src/App.js
@@ -1,38 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router} from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Navbar from './components/navbar';
-import EditObjectName from './components/edit-objectname';
-import ImagesList from './components/images-list';
-import AnnotationsList from './components/annotations-list';
-import DisplayAnnotationsBox from './components/display-annotationsbox';
-
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Navbar from "./components/navbar";
+import EditObjectName from "./components/edit-objectname";
+import ImagesList from "./components/images-list";
+import AnnotationsList from "./components/annotations-list";
+import DisplayAnnotationsBox from "./components/display-annotationsbox";
 
 function App() {
   return (
     <Router>
       <div>
         <Navbar />
-        <div className="container-fluid">
-
-          <div className="row">
-            
-            <div className="col-sm-3 col-md-2 sidebar" id="right_bar">
-              <ul className="nav nav-sidebar"> 
-                <EditObjectName />               
-              </ul>
-              <ul className="nav nav-sidebar">
-                <ImagesList />
-              </ul>
-            </div>
-
+        <div className="grid-container">
+          <div id="right_bar">
+            <ul className="nav nav-sidebar">
+              <EditObjectName />
+            </ul>
+            <ul className="nav nav-sidebar">
+              <ImagesList />
+            </ul>
+          </div>
+          <div>
             <DisplayAnnotationsBox />
+          </div>
 
-            <div className="col-md-3" id="right_bar" >
-              <AnnotationsList />
-            </div>
-
+          <div id="right_bar">
+            <AnnotationsList />
           </div>
         </div>
       </div>
