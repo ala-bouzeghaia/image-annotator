0a42c9fd2c200222cffc2c166988d1371244669b
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-31 20:57:31 +0100

    Changed width to be responsive

--- src/components/images-list.js
+++ src/components/images-list.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from "react";
 
 const ImagesList = () => {
-    return(
-        <div id="next_images">
-            <h5>Images: </h5>
-        </div>
-    );
+  return (
+    <div id="next_images" style={{ width: "100%" }}>
+      <h5>Images: </h5>
+    </div>
+  );
 };
 
 export default ImagesList;
