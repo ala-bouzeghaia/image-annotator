6eae1461225bbd03b3533f3947543b2d4c9fba3c
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-10-31 20:57:37 +0100

    Changed width to be responsive

--- src/components/edit-objectname.js
+++ src/components/edit-objectname.js
@@ -1,52 +1,63 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const EditObjectName = () => {
-    const [objectname, setObjectname] = useState('');
-    const [objects, setObjects] = useState([]);
-    
-    const onChangeObjectname = (e) => {
-        setObjectname(e.target.value);
-    };
+  const [objectname, setObjectname] = useState("");
+  const [objects, setObjects] = useState([]);
 
+  const onChangeObjectname = (e) => {
+    setObjectname(e.target.value);
+  };
 
-    const RenderObj = () => {
-        return( objects.map(obj => {
-            return(
-                <div className="radio" >               
-                    <input type="radio"  name="optionsRadios" id="optionsRadios1" checked/>
-                    <label className="label_radio" for="optionsRadios1">   {obj} </label>
-                    <br />
-                </div>
-            );
-        }));
-        
-    };    
-
-    const onSubmit = (e) => {
-        e.preventDefault();
-        setObjects([...objects, objectname])
-
-        setObjectname(''); 
-    }
-
-    return (
-
-        <div id="object_types">
-            <h5>Objects:</h5>
-            <input type="text" 
-                       required
-                       size="8" 
-                       className="text_input"
-                       value={objectname} 
-                       onChange={onChangeObjectname} />
-            <input type="submit" value="+" className="btn btn-primary" onClick={onSubmit} />
-            <br/>
-            {RenderObj()}
-
+  const RenderObj = () => {
+    return objects.map((obj) => {
+      return (
+        <div className="radio">
+          <input
+            type="radio"
+            name="optionsRadios"
+            id="optionsRadios1"
+            checked
+          />
+          <label className="label_radio" for="optionsRadios1">
+            {" "}
+            {obj}{" "}
+          </label>
+          <br />
         </div>
-              
-        
-    );
+      );
+    });
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setObjects([...objects, objectname]);
+
+    setObjectname("");
+  };
+
+  return (
+    <div id="object_types" style={{ width: "100%" }}>
+      <h5>Objects:</h5>
+      <input
+        type="text"
+        required
+        placeholder="Type the name of the object to detect"
+        size="8"
+        className="text_input"
+        value={objectname}
+        onChange={onChangeObjectname}
+      />
+      <input
+        type="submit"
+        value="+"
+        className="btn btn-primary"
+        onClick={onSubmit}
+        style={{ position: "right" }}
+      />
+      <br />
+      {RenderObj()}
+    </div>
+  );
 };
 
 export default EditObjectName;
