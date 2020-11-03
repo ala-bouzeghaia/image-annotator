98de52f3f20183e35f8e00f78abb83f7a8beab1b
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-11-03 20:15:49 +0100

    Added objectnames state to context api

--- src/components/bboxesContext.js
+++ src/components/bboxesContext.js
@@ -4,8 +4,11 @@ export const BboxesContext = createContext();
 
 export const BboxesProvider = (props) => {
   const [bboxes, setBboxes] = useState([]);
+  const [objects, setObjects] = useState([]);
   return (
-    <BboxesContext.Provider value={[bboxes, setBboxes]}>
+    <BboxesContext.Provider
+      value={{ boxes: [bboxes, setBboxes], objectNames: [objects, setObjects] }}
+    >
       {props.children}
     </BboxesContext.Provider>
   );
