a5ae8101b43531a52b8d67728da6a6d2ef382d76
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-11-03 16:39:13 +0100

    Added bboxes to annotation list

--- src/components/annotations-list.js
+++ src/components/annotations-list.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext } from "react";
+import { BboxesContext } from "./bboxesContext";
 
 const AnnotationsList = () => {
-    return(
-        <div id="annotated_box"> 
-            <h5>Annotations: </h5>
-        </div>
-    );
+  const [bboxes, setBboxes] = useContext(BboxesContext);
+  return (
+    <div id="annotated_box">
+      <h5>Annotations: </h5>
+      {bboxes.map((bb) => {
+        return (
+          <p>
+            [Left: {bb.left}, Top: {bb.top} , Width: {bb.width}, Height:{" "}
+            {bb.height}]
+          </p>
+        );
+      })}
+    </div>
+  );
 };
 
 export default AnnotationsList;
