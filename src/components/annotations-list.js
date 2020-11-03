54a7add35edb527d3f1432357626b288738697e6
ala-bouzeghaia <ala.bouzeghaia@gmail.com>
2020-11-03 20:16:54 +0100

    Added objectname state with context hook

--- src/components/annotations-list.js
+++ src/components/annotations-list.js
@@ -2,13 +2,23 @@ import React, { useContext } from "react";
 import { BboxesContext } from "./bboxesContext";
 
 const AnnotationsList = () => {
-  const [bboxes, setBboxes] = useContext(BboxesContext);
+  const { boxes, objectNames } = useContext(BboxesContext);
+  const [bboxes, setBboxes] = boxes;
+  const [objects, setObjects] = objectNames;
+  console.log(objects);
   return (
     <div id="annotated_box">
       <h5>Annotations: </h5>
       {bboxes.map((bb) => {
         return (
           <p>
+            {objects.map((obj) => {
+              return document.getElementById(`${obj}`) === null
+                ? ""
+                : document.getElementById(`${obj}`).checked
+                ? `${obj}`
+                : "";
+            })}
             [<b>Left:</b> {bb.left}, <b>Top:</b> {bb.top} , <b>Width:</b>{" "}
             {bb.width}, <b>Height:</b> {bb.height}]
           </p>
